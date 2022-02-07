2b320dddcd6443fb4db043f3381cf830663526d3
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2022-02-07 16:07:07 -0500

    Add files via upload

---
+++ Client/src/components/frontPageComponents/frontPage.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { Redirect } from "react-router-dom"
+import './frontPage.scss';
+import apiClient from '../../api/apiclient'
+
+function FrontPage() {
+    
+}
