fa84750ef801188fa7816aa4699be00700dd7445
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2022-03-03 13:06:27 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import FrontPage from './components/frontPageComponents/frontPage';
+import SubmitPage from './components/submitPageComponents/submitPage';
+import Header from './components/headerComponents/header'
+import {
+  Routes,
+  Route
+} from "react-router-dom"; 
+import './App.css';
+
+const App = () =>{
+ 
+    return (       
+      <div className="app">
+        <Routes>
+            <Header />        
+            <Route exact path = '/' element={<FrontPage/> }/>
+            <Route path="/SubmitPage" element={<SubmitPage/> }/>    
+          
+        </Routes> 
+ 
+      
+     </div>
+        )
+  
+        
+    
+    
+}
+
+export default App;
