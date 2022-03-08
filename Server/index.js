b398ed61191db320374fff00b89e595c3ad9f8e4
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2022-03-08 15:43:25 -0500

    Add files via upload

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,30 @@
-import App from "../Client/src/App";
+const express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+const mysql = require('mysql')
+var cors = require('cors')
+const { json } = require('body-parser')
+app.use(cors())
+
+const PORT = process.env.PORT || 8080
+
+var con = mysql.createConnection({
+  host: "0.0.0.0",
+  user: "root",
+  password: "Steven1spielberg",
+  port: "3306",
+  database: "forum"
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at port ${PORT}`)
+})
 App
