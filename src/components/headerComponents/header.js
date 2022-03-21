597976f9516e4a637060fdca4aa8f5ab14ce0089
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2022-03-21 15:46:52 -0400

    Add files via upload

---
+++ src/components/headerComponents/header.js
@@ -0,0 +1,8 @@
+import './header.scss';
+
+function Header() {
+return(
+ <div>hello</div>
+)
+}
+export default Header
---
+++ src/components/submitPageComponents/submitPage.js
@@ -0,0 +1,57 @@
+import './submitPage.scss';
+
+const [link, setLink] = useState ('')
+ const [title, setTitle] = useState ('')
+ const [doRedirect, setDoRedirect] = useState(false);
+
+ const onSubmit = (event) => {
+  event.preventDefault()
+  
+  if (link.length < 1) {
+     return document.getElementById('error').innerHTML = ("Please enter a Link");
+    }
+    if (title.length < 1) {
+      return  document.getElementById('error').innerHTML = ("Please enter a Title");
+    }
+     apiClient.signUp(link, title) .then((response) => {
+       console.log(response)
+       if (response === 'Link Created') {
+         return setDoRedirect(true);
+        }
+     })
+       .catch(error => {
+        console.log('Error found when creating meeting');
+    })
+     if (doRedirect) return <Navigate to="/FrontPage"/>;
+ }
+
+
+
+function SubmitPage() {
+return(
+  <section class="index-banner">
+  <div className= "bg"></div>
+  <div className= "float">
+    <div className= "type">
+      <div className="title">Submit a link</div>
+    <div className="label-1">
+      <label>Link </label> 
+      <input  type="text" name="link" onChange = {e => {setLink(e.target.value)}}></input> 
+    </div>
+    <div className="label-2">   
+      <label>Title </label>
+      <input  type="text" name="title" onChange = {e => {setTitle(e.target.value)}}></input>
+    </div>
+    <div id="error" className = "error"> </div>
+    <div>
+
+        <button >Submit</button>
+    </div>
+      </div>
+  </div>
+</section>         
+)
+}
+export default SubmitPage
+
+
